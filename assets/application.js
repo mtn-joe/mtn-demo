4286f0cf348e2ff955cf1003a659db43b93a6126
mtn-joe <izviagin@mtnhausdigital.com>
2023-06-21 18:40:53 +0400

    lesson 22 (404)

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,11 @@
 // Put your application javascript here
+window.addEventListener('DOMContentLoaded', (e) => {
+  if (document.getElementById('sort_by') != null) {
+    document.querySelector('#sort_by').addEventListener('change', (e) => {
+      const url = new URL(window.location.href);
+      url.searchParams.set('sort_by', e.currentTarget.value);
+
+      window.location = url.href;
+    });
+  }
+});
